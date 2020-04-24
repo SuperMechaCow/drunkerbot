d08f284db09ddd990d8550e194d2d51d36c7217e
Christopher Smith <csmith@ytech.edu>
2020-04-23 21:31:07 -0400

    1 hour warning

--- modules/playtest_handler.js
+++ modules/playtest_handler.js
@@ -12,7 +12,7 @@ const config = require('../modules/config.js');
 let discordClient = {}
 
 function checkTimes() {
-	db.all(`SELECT * FROM t_playtests WHERE status = 'accepted';`, function(error, results) {
+	db.all(`SELECT * FROM t_playtests WHERE status = '1hour';`, function(error, results) {
 		if (results != undefined) {
 			results.forEach(function(result, index) {
 				//Check each result
@@ -31,7 +31,7 @@ function checkTimes() {
 								if (runError) {
 									logger.error(runError)
 								} else {
-									startPlaytest(result);
+									startPlaytest(result, 'live');
 								}
 							});
 						}
@@ -40,9 +40,25 @@ function checkTimes() {
 			})
 		}
 	});
+	db.all(`SELECT * FROM t_playtests WHERE status = 'accepted';`, function(error, results) {
+		if (results != undefined) {
+			results.forEach(function(result, index) {
+				//Check each result
+				if (moment.unix(result.time).isSameOrBefore(moment().add(1, 'hours'))) {
+					db.run(`UPDATE t_playtests SET status = '1hour' WHERE id = ${result.id};`, function(runError) {
+						if (runError) {
+							logger.error(runError)
+						} else {
+							startPlaytest(result, '1hour');
+						}
+					});
+				}
+			})
+		}
+	});
 }
 
-function startPlaytest(mapTest) {
+function startPlaytest(mapTest, announce) {
 	request.post({
 		url: 'https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/',
 		form: {
@@ -60,24 +76,30 @@ function startPlaytest(mapTest) {
 				logger.error("There's a problem with the Steam Connection");
 			}
 		} else {
-			logger.verbose("Playtest live");
 			let utcOS = 0;
 			if (moment().isDST()) {
 				utcOS = -240;
 			} else {
 				utcOS = -300;
 			}
-			let time = moment(mapTest.time).utcOffset(utcOS).format("HH:mm DD/MM/YY");
+			let time = moment.unix(mapTest.time).utcOffset(utcOS).format("HH:mm DD/MM/YY");
 			let data = JSON.parse(body).response.publishedfiledetails[0];
 			let playtestRole = discordClient.guilds.cache.get(mapTest.guildDID).roles.cache.find(role => role.name === 'Playtester');
 			var embed = new Discord.MessageEmbed();
-			embed.setTitle("Playtest is live:")
-				.addField("Map Name:", data.title)
-				.addField("Playtest Started:", time)
+			if (announce == 'live') {
+				logger.verbose("Playtest live: " + data.title);
+				embed.setTitle("Playtest is live:")
+				.setColor('GREEN');
+			} else if (announce == '1hour') {
+				logger.verbose("Playtest starting soon: " + data.title);
+				embed.setTitle("Playtest starting soon:")
+				.setColor('YELLOW');
+			}
+			embed.addField("Map Name:", data.title)
+				.addField("Playtest Starts:", time)
 				.addField("Description:", data.description)
 				.addField("Gamemode being tested:", mapTest.type)
 				.setImage(data.preview_url)
-				.setColor('RED')
 				.setURL(`${settings.urlWEB}steamgame?ip=${config.GAMESERV_IP}&port=${config.GAMESERV_PORT}`);
 			//.setURL(`steam://connect/${config.GAMESERV_IP}:${config.GAMESERV_PORT}`); //This doesn't work >:[
 			let mapChannel = discordClient.guilds.cache.get(mapTest.guildDID).channels.cache.find(channel => channel.name === 'playtest');
